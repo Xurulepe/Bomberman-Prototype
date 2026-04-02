250765376442f8cf2a4b7fd271654f1ae6fe6752
Xurulepe <xurulepe@gmail.com>
2026-04-01 22:21:59 -0300

    Bomb explosion
    + Bomba explodindo nas quatro direções e detectando apenas objetos que podem ser explodidos usando layer mask.

--- Assets/Scripts/Bombs/Bomb.cs
+++ Assets/Scripts/Bombs/Bomb.cs
@@ -4,7 +4,16 @@ public class Bomb : MonoBehaviour
 {
     [Header("Bomb Settings")]
     [SerializeField] private float explosionDelay = 3f;
-    [SerializeField] private float explosionTimer = 0f;
+    [SerializeField] private int explosionDistance = 3;
+    [SerializeField] private LayerMask explosionLayerMask;
+
+    private float explosionTimer = 0f;
+    private Collider2D[] objectsHit;
+
+    private void OnEnable()
+    {
+        explosionTimer = 0f;
+    }
 
     private void Update()
     {
@@ -13,15 +22,47 @@ public class Bomb : MonoBehaviour
         if (explosionTimer >= explosionDelay)
         {
             Explode();
-
-            explosionTimer = 0f;
         }
     }
 
     private void Explode()
     {
-        print("Boom! The bomb has exploded.");
-        
+        objectsHit = GetObjectsInExplosionRange();
+
+        foreach (Collider2D hit in objectsHit)
+        {
+            Debug.Log("Hit object: " + hit.name);
+        }
+
         gameObject.SetActive(false);
+    }
+
+    private Collider2D[] GetObjectsInExplosionRange()
+    {
+        Collider2D[] allObjectsInExplosionRange;
+        Collider2D[] objectsInHorizontalRange = Physics2D.OverlapBoxAll(transform.position, new Vector2(explosionDistance * 2f, 1f), 0f, explosionLayerMask);
+        Collider2D[] objectsInVerticalRange = Physics2D.OverlapBoxAll(transform.position, new Vector2(1f, explosionDistance * 2f), 0f, explosionLayerMask);
+
+        int allObjectsCount = objectsInHorizontalRange.Length + objectsInVerticalRange.Length;
+        allObjectsInExplosionRange = new Collider2D[allObjectsCount];
+
+        for (int i = 0; i < objectsInHorizontalRange.Length; i++)
+        {
+            allObjectsInExplosionRange[i] = objectsInHorizontalRange[i];
+        }
+
+        for (int i = 0; i < objectsInVerticalRange.Length; i++)
+        {
+            allObjectsInExplosionRange[objectsInHorizontalRange.Length + i] = objectsInVerticalRange[i];
+        }
+
+        return allObjectsInExplosionRange;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector2(explosionDistance * 2f, 1f));
+        Gizmos.DrawWireCube(transform.position, new Vector2(1f, explosionDistance * 2f));
     }
 }
