465938c7fca26ed604667e18be2ef43bec433085
Xurulepe <xurulepe@gmail.com>
2026-04-02 21:01:27 -0300

    Bomb explosion
    = Ajustado explosão da bomba para não atravessar paredes destrutíveis e indestrutíveis.
    + Bombas agora podem explodir outras bombas dentro do alcance.

--- Assets/Scripts/Bombs/Bomb.cs
+++ Assets/Scripts/Bombs/Bomb.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
+public class Bomb : MonoBehaviour, IDestroyable
 {
     [Header("Bomb Settings")]
     [SerializeField] private float explosionDelay = 3f;
     [SerializeField] private int explosionDistance = 3;
-    [SerializeField] private LayerMask explosionLayerMask;
+    [SerializeField] private float explosionThickness = 0.7f;
+    [SerializeField] private LayerMask explosionLayerMask;  // damageable & destructible objects + player layers
     [SerializeField] private LayerMask playerLayerMask;
+    [SerializeField] private LayerMask obstaclesLayerMask;  // destructible and indestructible objects
 
     private float explosionTimer = 0f;
+    private bool hasExploded = false;
     private Collider2D[] objectsHit;
 
     private void OnEnable()
     {
         explosionTimer = 0f;
+        hasExploded = false;
 
         StartCoroutine(IgnoreCollisionWithPlayersUntilExit());
     }
@@ -29,12 +33,27 @@ public class Bomb : MonoBehaviour
         }
     }
 
+    public void Destroy()
+    {
+        if (!hasExploded)
+        {
+            Explode();
+        }
+    }
+
     private void Explode()
     {
+        hasExploded = true;
+
         objectsHit = GetObjectsInExplosionRange();
 
         foreach (Collider2D hit in objectsHit)
         {
+            if (hit == null || hit.gameObject == gameObject)
+            {
+                continue;
+            }
+
             Debug.Log("Hit object: " + hit.name);
 
             if (hit.TryGetComponent(out IDestroyable destroyable))
@@ -49,23 +68,65 @@ public class Bomb : MonoBehaviour
     private Collider2D[] GetObjectsInExplosionRange()
     {
         Collider2D[] allObjectsInExplosionRange;
-        Collider2D[] objectsInHorizontalRange = Physics2D.OverlapBoxAll(transform.position, new Vector2(explosionDistance * 2f, 1f), 0f, explosionLayerMask);
-        Collider2D[] objectsInVerticalRange = Physics2D.OverlapBoxAll(transform.position, new Vector2(1f, explosionDistance * 2f), 0f, explosionLayerMask);
 
-        int allObjectsCount = objectsInHorizontalRange.Length + objectsInVerticalRange.Length;
+        int rightRangeExplosionDistance = CountCells(Vector2.right);
+        int leftRangeExplosionDistance = CountCells(Vector2.left);
+        int upRangeExplosionDistance = CountCells(Vector2.up);
+        int downRangeExplosionDistance = CountCells(Vector2.down);
+
+        Vector3 startPosition;
+
+        startPosition = transform.position + Vector3.right * rightRangeExplosionDistance * 0.5f;
+        Collider2D[] objectsInRightRange = Physics2D.OverlapBoxAll(startPosition, new Vector2(rightRangeExplosionDistance, explosionThickness), 0f, explosionLayerMask);
+
+        startPosition = transform.position + Vector3.left * leftRangeExplosionDistance * 0.5f;
+        Collider2D[] objectsInLeftRange = Physics2D.OverlapBoxAll(startPosition, new Vector2(leftRangeExplosionDistance, explosionThickness), 0f, explosionLayerMask);
+
+        startPosition = transform.position + Vector3.up * upRangeExplosionDistance * 0.5f;
+        Collider2D[] objectsInUpRange = Physics2D.OverlapBoxAll(startPosition, new Vector2(explosionThickness, upRangeExplosionDistance), 0f, explosionLayerMask);
+
+        startPosition = transform.position + Vector3.down * downRangeExplosionDistance * 0.5f;
+        Collider2D[] objectsInDownRange = Physics2D.OverlapBoxAll(startPosition, new Vector2(explosionThickness, downRangeExplosionDistance), 0f, explosionLayerMask);
+
+        int allObjectsCount = objectsInRightRange.Length + objectsInLeftRange.Length + objectsInUpRange.Length + objectsInDownRange.Length;
         allObjectsInExplosionRange = new Collider2D[allObjectsCount];
 
-        for (int i = 0; i < objectsInHorizontalRange.Length; i++)
+        for (int i = 0; i < objectsInRightRange.Length; i++)
         {
-            allObjectsInExplosionRange[i] = objectsInHorizontalRange[i];
+            allObjectsInExplosionRange[i] = objectsInRightRange[i];
         }
 
-        for (int i = 0; i < objectsInVerticalRange.Length; i++)
+        for (int i = 0; i < objectsInLeftRange.Length; i++)
         {
-            allObjectsInExplosionRange[objectsInHorizontalRange.Length + i] = objectsInVerticalRange[i];
+            int index = objectsInRightRange.Length + i;
+            allObjectsInExplosionRange[index] = objectsInLeftRange[i];
+        }
+
+        for (int i = 0; i < objectsInUpRange.Length; i++)
+        {
+            int index = objectsInRightRange.Length + objectsInLeftRange.Length + i;
+            allObjectsInExplosionRange[index] = objectsInUpRange[i];
+        }
+
+        for (int i = 0; i < objectsInDownRange.Length; i++)
+        {
+            int index = objectsInRightRange.Length + objectsInLeftRange.Length + objectsInUpRange.Length + i;
+            allObjectsInExplosionRange[index] = objectsInDownRange[i];
         }
 
         return allObjectsInExplosionRange;
+    }
+
+    private int CountCells(Vector2 direction)
+    {
+        RaycastHit2D firstObstacle = Physics2D.Raycast(transform.position, direction, explosionDistance, obstaclesLayerMask);
+
+        if (firstObstacle.collider != null)
+        {
+            return Mathf.RoundToInt(Vector2.Distance(transform.position, firstObstacle.collider.transform.position));
+        }
+
+        return explosionDistance;
     }
 
     private IEnumerator IgnoreCollisionWithPlayersUntilExit()
@@ -89,7 +150,7 @@ public class Bomb : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector2(explosionDistance * 2f, 1f));
-        Gizmos.DrawWireCube(transform.position, new Vector2(1f, explosionDistance * 2f));
+        Gizmos.DrawWireCube(transform.position, new Vector2(explosionDistance * 2f, explosionThickness));
+        Gizmos.DrawWireCube(transform.position, new Vector2(explosionThickness, explosionDistance * 2f));
     }
 }
